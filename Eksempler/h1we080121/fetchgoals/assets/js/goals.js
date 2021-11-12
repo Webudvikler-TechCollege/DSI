9f93b1d93a4d8f18bf87de3227b85cbca0b2152c
Heinz K <heka@techcollege.dk>
2021-11-12 13:02:07 +0100

    Goal og Helper Comments

--- Eksempler/h1we080121/fetchgoals/assets/js/goals.js
+++ Eksempler/h1we080121/fetchgoals/assets/js/goals.js
@@ -1,25 +1,61 @@
-// Funktioner til det specifikke projekt
+/**
+ * Fil med specifikke funktioner 
+ * Bruges til verdensmål
+ */
+
+/* Importerer myFetch funktion fra helper fil */
 import { myFetch } from './helper.js';
 
+/* Funktionskald til at hente liste med mål */
+getGoalList();
+
+/**
+ * Funktionsvariabel til at hente liste af mål
+ */
 const getGoalList = async () => {
+    // Kalder data
     const data = await myFetch('https://api.mediehuset.net/sdg/goals');
+
+    // Mapper data
     data.items.map(function(item, key) {
+        // Definerer div wrapper
         const wrapper = document.createElement('div');
+        // Definerer anchor tag med tekst og click event
         const link = document.createElement('a');
         link.innerText = item.title;
+
+        // Click event kalder detalje funktion med målets id
         link.addEventListener('click', () => {
             getGoalDetails(item.id);
         })
+
+        // Appender link og wrapper
         wrapper.append(link);
         document.querySelector('.goalcontainer').append(wrapper);
     }) 
 }
 
-getGoalList();
-
+/**
+ * Funktionsvariabel til at hente mål detaljer
+ * @param {number} goal_id 
+ */
 const getGoalDetails = async goal_id => {
+    // Kalder data
     const data = await myFetch(`https://api.mediehuset.net/sdg/goals/${goal_id}`);
+
+    // Definerer titel element
     const title = document.createElement('h1');
     title.innerText = data.item.title;
+
+    // Definerer div element til svg html
+    const icon = document.createElement('div');
+    icon.innerHTML = data.item.icon;
+
+    // Henter svg ind som et js dom objekt 
+    const svg = icon.querySelector('svg');
+    svg.style.fill = `#${data.item.color}`;
+
+    // Appender elementer til .goalcontainer element
     document.querySelector('.goalcontainer').append(title);
+    document.querySelector('.goalcontainer').append(icon);
 }
--- Eksempler/h1we080121/fetchgoals/assets/js/helper.js
+++ Eksempler/h1we080121/fetchgoals/assets/js/helper.js
@@ -1,5 +1,19 @@
-// Global funktioner - kan bruges overalt
+/**
+ * Fil med globale funktioner
+ * 
+ * Keywordet export gør at alle 
+ * funktioner kan ekporteres individuelt.
+ * For at kunne anvende denne metode
+ * i Vanilla Javascript skal script 
+ * tagget have attributten type 
+ * med værdien module
+ */ 
 
+/**
+ * Funktion til globale fetch kald
+ * @param {string} url 
+ * @returns json object
+ */
 export const myFetch = async (url) => {
     try {
         const response = await fetch(url);
