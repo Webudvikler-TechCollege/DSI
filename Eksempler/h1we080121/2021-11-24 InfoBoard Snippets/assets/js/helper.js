2df968afe936e94ce63cdfe2726ee2dfca657f48
Heinz K <heka@techcollege.dk>
2021-11-26 12:54:05 +0100

    Aktiviteter - Update med JSON config fil

--- Eksempler/h1we080121/2021-11-24 InfoBoard Snippets/assets/js/helper.js
+++ Eksempler/h1we080121/2021-11-24 InfoBoard Snippets/assets/js/helper.js
@@ -37,12 +37,93 @@ export const myFetch = async (url, options = null) => {
 }
 
 /**
- * 
- * @param {String} date 
+ * Funktion til at hente dk dag
+ * @param {*} intDay 
  * @returns 
  */
- export const convert2Timezone = date => {
-    const d = new Date(date);    
-    const stamp = ((d.valueOf()/1000) - (d.getTimezoneOffset()*60))*1000;
-    return new Date(stamp);
+ export const day2local = intDay => {
+    let day;
+    switch(intDay) {
+        default:
+            break;
+        case 0:
+            day = 'Søndag';
+            break;
+        case 1:
+            day = 'Mandag';
+            break;
+        case 2:
+            day = 'Tirsdag';
+            break;
+        case 3:
+            day = 'Onsdag';
+            break;
+        case 4:
+            day = 'Torsdag';
+            break;
+        case 5:
+            day = 'Fredag';
+            break;
+        case 6:
+            day = 'Lørdag';
+            break;
+    }
+
+    return day;
+}
+
+/**
+ * Funktion til at hente dk måned
+ * @param {*} intMonth 
+ * @returns 
+ */
+export const month2local = intMonth => {
+    let month;
+    switch (intMonth) {
+        default:
+            break;
+        case 0:
+            month = 'Januar';
+            break;
+        case 1:
+            month = 'Febuar';
+            break;
+        case 2:
+            month = 'Marts';
+            break;
+        case 3:
+            month = 'April';
+            break;
+        case 4:
+            month = 'Maj';
+            break;
+        case 5:
+            month = 'Juni';
+            break;
+        case 6:
+            month = 'Juli';
+            break;
+        case 7:
+            month = 'August';
+            break;
+        case 8:
+            month = 'September';
+            break;
+        case 9:
+            month = 'Oktober';
+            break;
+        case 10:
+            month = 'November';
+            break;
+        case 11:
+            month = 'December';
+            break;
+    }   
+    
+    return month;
+}
+
+export const dayDateMonth2local = strDate => {
+    const date = new Date(strDate);
+    return `${day2local(date.getDay())} d. ${date.getDate()}. ${month2local(date.getMonth())}`;
 }
