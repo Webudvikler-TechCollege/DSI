4d0f7bedffe865e63b0f0555c886e4fe325adc24
Webudvikler Hovedforløb <heka@techcollege.dk>
2023-06-01 13:40:25 +0200

    Form Body Settings

--- Sites/HF - Juni 2023/Routes/post.router.js
+++ Sites/HF - Juni 2023/Routes/post.router.js
@@ -1,13 +1,33 @@
+// Importerer dependencies
 import express from 'express'
 
+// Deklarerer var til router
 const postRouter = express.Router()
 
+// Route med GET method - henter
 postRouter.get('/posts', (req, res) => {
 	res.send('Hent alle poster')
 })
+
+// Route med POST method - opretter
 postRouter.post('/posts', (req, res) => {
-	console.log(req.body);
-	res.send('Opret ny post')
+	// Henter form body data på res objektet og "sender" (udskriver) til browser
+	res.send(`
+		${req.body.firstname} ${req.body.lastname}
+		${req.body.address}
+		${req.body.city}
+	`)
 })
+
+// Route med PUT method - opdaterer
+postRouter.put('/posts', (req, res) => {
+	res.send('Opdater post')
+})
+
+// Route med DELETE method - Sletter
+postRouter.delete('/posts', (req, res) => {
+	res.send('Sletter post')
+})
+
 
 export { postRouter }
--- Sites/HF - Juni 2023/index.js
+++ Sites/HF - Juni 2023/index.js
@@ -1,21 +1,31 @@
+// Importerer dependencies
 import express from 'express'
 import { postRouter } from './Routes/post.router.js';
-import dotenv from 'dotenv'
 
+// Importerer og sætter dotenv til globale vars
+import dotenv from 'dotenv'
 dotenv.config()
 
+// Deklarerer app var med ekspress objekt
 const app = express()
 
+// Udvider app så vi kan læse form body data
+app.use(express.urlencoded({ extended: true }))
+
+// Kalder root route - forsiden af vores app 
 app.get("/", (req, res) => {
 	res.send("Velkommen til min NodeJS app");
 })
 
+// Anvender eksterne routes
 app.use(postRouter)
 
+// Skriver fejl hvis route ikke findes
 app.use((req, res) => {
     res.status(404).send("Siden blev ikke fundet!")
 })
 
+// Aktiverer server og lytter på port fra .env fil
 app.listen(process.env.PORT, () => {
 	console.log(`Server kører på http://localhost:${process.env.PORT}`)	
 })
